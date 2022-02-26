7b87548bc538e327ef6bb6a2da87bdc6affaf74f
LeonidevFR <leonardploton.dev@gmail.com>
2022-02-26 14:42:15 +0100

    Adding axios to the project for API calls

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios'
 import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
 
+Vue.prototype.$http = axios
 Vue.use(Buefy)
 
 Vue.config.productionTip = false
