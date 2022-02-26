2de42911097d9aa9157b1ad4c7dcc97fa1fe1667
LeonidevFR <leonardploton.dev@gmail.com>
2022-02-26 14:26:45 +0100

    init project with Prettier / Buefy / Bulma

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <TheHomePage />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TheHomePage from "./js/pages/TheHomePage.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    TheHomePage,
+  },
+};
 </script>
 
 <style>
---
+++ src/js/pages/TheHomePage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1 class="title is-1">Je suis le titre</h1>
+    <h2 class="subtitle is-3">Je suis un sous-titre</h2>
+    <p>Je suis un contenu</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "the-home-page",
+};
+</script>
+
+<style></style>
