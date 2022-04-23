cb8490bf7fea2d11404cac0abaa5ace2617af527
LeonidevFR <leonardploton.dev@gmail.com>
2022-04-23 17:17:35 +0200

    App with API call and filters

---
+++ src/js/components/ActivityComponent.vue
@@ -0,0 +1,72 @@
+<template>
+  <div v-if="Object.entries(idea).length !== 0" class="mt-6 activity-container">
+    <div v-if="!idea.error" class="hero activity-body is-primary">
+      <div class="hero-body">
+        <p class="subtitle is-4 has-text-weight-semibold mb-6">You could :</p>
+        <h2 class="title is-2 mt-4">{{ idea.activity }}</h2>
+        <div class="columns mt-6">
+          <div
+            v-if="idea.price || idea.price === 0"
+            class="column is-4 subtitle is-4 has-text-weight-semibold"
+          >
+            Price : {{ calculatedPrice }}/10
+          </div>
+          <div
+            v-if="idea.type"
+            class="column is-4 subtitle is-4 has-text-weight-semibold"
+          >
+            Category : {{ idea.type }}
+          </div>
+          <div
+            v-if="idea.participants"
+            class="column is-4 subtitle is-4 has-text-weight-semibold"
+          >
+            Participants : {{ idea.participants }}
+          </div>
+        </div>
+        <a
+          v-if="idea.link"
+          :href="idea.link"
+          class="subtitle is-4 has-text-link"
+          >{{ idea.link }}
+        </a>
+      </div>
+    </div>
+    <div v-else>
+      <h1 class="title is-4 has-text-danger">Whoops, {{ idea.error }}</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "activity-component",
+  props: {
+    idea: {
+      type: Object,
+    },
+  },
+  computed: {
+    calculatedPrice() {
+      if (this.idea.price !== 0) {
+        return this.idea.price * 10;
+      } else {
+        return 0;
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+.activity-container {
+  display: flex;
+  justify-content: center;
+  border-radius: 8px;
+}
+
+.activity-body {
+  width: 90%;
+  border-radius: 8px;
+}
+</style>
---
+++ src/js/components/HomePageHeader.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <section class="hero is-primary">
+      <div class="hero-BODY">
+        <div class="columns is-vcentered">
+          <div class="column is-4">
+            <figure class="image is-96x96">
+              <a href="/">
+                <img src="../../assets/images/bored-svgrepo-com.svg" />
+              </a>
+            </figure>
+          </div>
+          <div class="column is-4">
+            <h1 class="title is-1">I'm so Bored</h1>
+            <h2 class="subtitle is-4">What should I do ?</h2>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "home-page-header",
+};
+</script>
+
+<style scoped>
+.hero {
+  padding: 20px;
+}
+figure {
+  filter: invert(1) sepia(1) saturate(5) hue-rotate(175deg);
+}
+</style>
